36abe7264b52a9698e45603f45f3806c94a35287
Jonathan T Dean <jonathantomdean@gmail.com>
2020-04-21 21:31:30 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,32 @@
-# Write your methods here
+# Loop Message 5 Times
+def loop_message_five_times(message)
+  # Counter
+  i = 0
+  # Looper
+  while i < 6
+    puts message
+    i += 1
+  end
+end
+
+# Loop Message n Amount of Times
+def loop_message_n_times(message, integer)
+  # Counter
+  i = 0
+  # Looper
+  until i == integer
+    puts message
+    i += 1
+  end
+end
+
+# Output each element in array
+def output_array(array)
+  array.each do |i|
+    puts i
+  end
+end
+
+# Get an array of elements, and converts elements to strings and 
+# Returns a new array from 
+# def 
